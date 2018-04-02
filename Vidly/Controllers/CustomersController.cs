2cfe8ff50a6e8e6d77af3ca7c1e2076cffbe5e16
Omar <berraj97@gmail.com>
2018-04-02 21:53:48 +0200

    Solution to the First excercice

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null) return NotFound();
+
+            return View(customer);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id = 1, Name = "John smith"},
+                new Customer{Id = 2, Name = "Jeda pincket"}                                                                             
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        public IActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "customer 1"},
+                new Customer {Name = "customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel); // To render this view with the movie
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("movieId = " + movieId);
+        }
+    }
+}
